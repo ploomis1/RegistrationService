71f12a118d36390a408d1b67c9a286a6eebc37be
parker.loomis1 <parker.loomis1@gmail.com>
2021-06-10 11:56:11 -0400

    Creation

---
+++ src/main/java/com/revature/dmv/registration/beans/Registration.java
@@ -0,0 +1,9 @@
+package com.revature.dmv.registration.beans;
+
+public class Registration {
+
+	private String registration;
+	private String title;
+	private String username;
+	private int date;
+}
